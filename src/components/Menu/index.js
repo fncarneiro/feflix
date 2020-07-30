1c25872adda8f84f0b56c1a3703bbcd0ba971edd
Fernando Nascimento Carneiro <fernando.carneiro@dataprev.gov.br>
2020-07-30 17:58:21 -0300

    Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/feflix_logo.png';
 import './Menu.css';
 import Button from '../Button';
@@ -6,10 +7,10 @@ import Button from '../Button';
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="FeFlix logo"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
+            </Link>
+            <Button as="Link" className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>        
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './reset.css';
+import Home from './pages/Home/Home';
 
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from './pages/Cadastro/Video';
+import CadastroCategoria from './pages/Cadastro/Categoria';
+
+const Pagina404 = () => (<div>Página 404</div>);
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  <BrowserRouter>
+     <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/cadastro/video" component={CadastroVideo} />
+        <Route path="/cadastro/categoria" component={CadastroCategoria} />
+        <Route component={Pagina404} exact />
+     </Switch>
+     
+  </BrowserRouter>,
+
   document.getElementById('root')
 );
