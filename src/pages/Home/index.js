eab8b66f5013628e69c6fc20505d4226a1742818
Fernando Nascimento Carneiro <fernando.carneiro@dataprev.gov.br>
2020-08-01 10:49:18 -0300

    acertos ate a aula 3

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,7 @@ function Home() {
       <BannerMain 
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é front-end"}
+        videoDescription={"Mergulhando no Batente das Agulhas"}
       />
 
       <Carousel 
